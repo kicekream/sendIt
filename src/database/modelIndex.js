dcfc3dcec5120b05fe55e52d76457b3024ee4a76
kicekream <kicekream@gmail.com>
2021-08-20 23:53:30 +0100

    edited ssl settings for modelIndex

--- src/database/modelIndex.js
+++ src/database/modelIndex.js
@@ -3,11 +3,13 @@ import {} from "dotenv/config";
 
 // import models from "./models";
 
-const string = process.env.NODE_ENV === 'test'
-? process.env.TEST_DATABASE_URL
-: process.env.DATABASE_URL;
+const string =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
 
-let connect = { connectionString: string };
+let connect = { connectionString: string, 
+    ssl: { rejectUnauthorized: false } };
 
 const pool = new Pool(connect);
 
